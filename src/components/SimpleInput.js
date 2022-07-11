6d733e2a193d68effab383c83f03acfc1f3b9710
aminmallol <aminm-34@hotmail.fr>
2022-07-11 17:03:08 +0100

    Added 3 different kinds of form validation (change / submit / blur). Works perfectly.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,48 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("")
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredNameInputIsTouched, setEnteredNameInputIsTouched] = useState(false)
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value)
+    if (e.target.value.trim() !== "") {
+      setEnteredNameIsValid(true)
+    }
+  }
+
+  const nameInputBlurHandler = (e) => {
+    setEnteredNameInputIsTouched(true)
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false)
+    }
+  }
+
+  const onSubmitUserNameHandler = (e) => {
+    e.preventDefault()
+    setEnteredNameInputIsTouched(true)
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false)
+      return
+    }
+    setEnteredNameIsValid(true)
+    console.log(enteredName);
+    setEnteredName("")
+    setEnteredNameInputIsTouched(false)
+  }
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameInputIsTouched
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={onSubmitUserNameHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
       </div>
+      {nameInputIsInvalid && <p className="error-text">Enter a valid name</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
